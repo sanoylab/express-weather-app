4a67e88216722f2b2f423d631e77d3fa844b0a96
Yonas Yeneneh <yonasyeneneh@Yonass-MBP.phub.net.cable.rogers.com>
2020-07-16 13:21:28 -0400

    this is a weather app that uses three API endpoints. First API used to get geocode, the second API used to get the weather forcast, the third API used to get image background for the searched location

---
+++ public/js/script.js
@@ -0,0 +1,86 @@
+console.log('client javascript file loaded')
+
+const form = document.querySelector('form');
+const address = document.querySelector('input')
+
+const locationName = document.querySelector('#locationName')
+const temparature = document.querySelector('#temprature')
+const feelslike =  document.querySelector('#feelslike')
+const imageIcon = document.querySelector('#imgIcon')
+const searchResult = document.querySelector('.info')
+
+const currentLocation = document.querySelector('#currentBtn')
+window.addEventListener("load", function(){
+
+    if(localStorage.getItem('location')){
+        console.log('yyyy')
+        featchData(localStorage.getItem('location').toString()) 
+    }
+})
+
+
+currentLocation.addEventListener('click', (e)=>{
+    e.preventDefault();
+    getLocation();
+})
+
+ const getLocation = ()=>{
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+  
+  const showPosition= (position) =>{
+      const latitude = position.coords.latitude 
+      const longitude = position.coords.longitude;
+      featchData(longitude+","+latitude)
+      
+  }
+
+
+
+searchResult.style.display = 'none'
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const location = address.value;
+   
+    featchData(location)
+
+   
+})
+
+
+const featchData = (location)=>{
+    console.log(location)
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error)
+            } else{
+                searchResult.style.display = 'block'
+                imageIcon.src=data.weatherIcon
+                locationName.textContent = data.location
+                temparature.textContent = `${data.temprature}˚ ${data.forcast}.`
+                feelslike.textContent = `Feels Like: ${data.feelsLike}˚ Humidity: ${data.humidity}˚`
+
+                fetch('https://api.unsplash.com/search/photos?client_id=ilTEI9kIDd6WytvhfVlaV_COEUcbJnxDh0kOQBJV_c8&page=1&query='+data.location).then((response)=>{
+                    response.json().then((data)=>{
+                        console.log(data.results[0].urls.full)
+                        let backImage = data.results[0].urls.regular
+                        document.body.style.backgroundImage = `url('${backImage}')`;
+                        document.body.style.backgroundColor = "#f3f3f3";
+                        document.body.style.backgroundSize = "100% 100%"
+                        document.body.style.backgroundRepeat = "no-repeat"
+
+                    })
+                })
+                localStorage.setItem('location', data.location)
+    
+            }
+            
+        })
+    })
+}
+
---
+++ src/app.js
@@ -0,0 +1,125 @@
+const express =  require('express')
+const path =  require('path')
+const hbs = require('hbs')
+
+const geocode = require('./util/geocode')
+const forcast = require('./util/forcast')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+//Define path for Express config
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectory))
+
+
+app.get('/', (req, res)=>{
+    res.render('index', {
+        title: 'New York',
+        author: 'Yonas Yeneneh'
+    })
+})
+app.get('/about', (req, res)=>{
+    res.render('about', {
+        title: 'About Me',
+         author: 'Yonas Yeneneh'
+    })
+})
+
+app.get('/help', (req, res)=>{
+    res.render('help', {
+        title: 'Help',
+        body: 'This is a text for the help page'
+    })
+})
+
+
+/*
+app.get('/', (req, res)=>{ 
+   res.send('<h1>Home</h1>')
+})
+app.get('/help', (req, res)=>{
+    res.send('<h1>Help</h1>')
+})
+app.get('/about', (req, res)=>{
+    res.send('<h1>About</h1>')
+})
+
+*/
+app.get('/json', (req, res)=>{
+    res.send([{
+        name: 'Yonas', 
+        age: 35,
+        children: 2,
+        married: 1
+    }, {
+        name: 'Lideya', 
+        age: 28,
+        children: 2,
+        married: 1
+    }])
+})
+
+app.get('/weather', (req, res)=>{
+    if(!req.query.address){
+      return  res.send({
+            'error': 'You must provide address'
+        })
+    }
+    geocode(req.query.address, (error, {latlong,placeName}={})=>{
+
+        
+        if(error){
+            return res.send({ error })
+        }
+        forcast(latlong, placeName, (error, forcastData)=>{
+            if(error){
+                return res.send({ error })
+            }
+
+              
+            res.send({
+                temprature: forcastData.temprature, 
+                forcast: forcastData.description,
+                feelsLike: forcastData.feelsLike,
+                weatherIcon: forcastData.weatherIcon,
+                humidity: forcastData.humidity,
+                location: forcastData.location,
+                address: req.query.address
+            })
+        })
+    })
+    
+   
+    
+
+ 
+    /*
+    res.render('weather', {
+        forcast: req.query.forcast,
+        location: req.query.location
+    })
+    */
+
+})
+
+app.get('*', (req, res)=>{
+    res.render('404')
+})
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log('Server started on port 3000')
+})
